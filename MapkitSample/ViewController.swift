1489cebe4976abf3907296579e8010f476a96893
gurumobile <sambo@Nikolas-Mac.local>
2017-05-31 16:33:32 +0300

    no message

---
+++ MapkitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapkitSample
+//
+//  Created by Nikola on 5/31/17.
+//  Copyright © 2017 Nikola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
